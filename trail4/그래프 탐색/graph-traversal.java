2a1832d8ec0d83d0b7369c7935bb3d0c41e2c8dc
Oknook <wjsguscjf23@naver.com>
2026-09-16 15:31:05 +0900

    [Passed] 그래프 탐색 (95ms, 9MB)

--- trail4/그래프 탐색/graph-traversal.java
+++ trail4/그래프 탐색/graph-traversal.java
@@ -2,13 +2,16 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static int N, M, ans = 0;
+    static boolean visited[], mat[][];
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-        boolean mat[][] = new boolean[N+1][N+1];
+        mat = new boolean[N+1][N+1];
+        visited = new boolean[N + 1];
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int head = Integer.parseInt(st.nextToken());
@@ -16,27 +19,23 @@ public class Main {
             mat[head][tail] = true;
             mat[tail][head] = true;
         }
-
-        boolean[] visited = new boolean[N + 1];
-        Queue<Integer> q = new LinkedList<>();
-
-        q.add(1);
-        visited[1] = true;
-
-        int ans = 0;
-
-        while (!q.isEmpty()) {
-            int v = q.poll();
-
-            for (int i = 1; i <= N; i++) {
-                if (mat[v][i] && !visited[i]) {
-                    visited[i] = true;
-                    q.add(i);
-                    ans++;
-                }
-            }
-        }
+        
+        dfs(1);
 
         System.out.println(ans);
+    }
+    static void dfs(int v) {
+
+        visited[v] = true;
+
+        for (int i = 1; i <= N; i++) {
+
+            if (mat[v][i] && !visited[i]) {
+
+                ans++;
+
+                dfs(i);
+            }
+        }
     }
 }

a3e889eb41908390e0247d57ea9ea1cea4e1e1da
Oknook <wjsguscjf23@naver.com>
2026-09-16 16:11:29 +0900

    [Passed] 최소 경로로 탈출하기 (94ms, 9MB)

---
+++ trail4/최소 경로로 탈출하기/escape-with-min-distance.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, M;
+    static boolean visited[][], mat[][];
+    static class Point {
+        int r, c, cnt;
+        Point(int r, int c, int cnt) {
+            this.r = r;
+            this.c = c;
+            this.cnt = cnt;
+        }
+    }
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        mat = new boolean[N][M];
+        visited = new boolean[N][M];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                mat[i][j] = Integer.parseInt(st.nextToken()) == 1;
+            }
+        }
+
+        System.out.println(bfs());
+    }
+
+    static int dr[] = {-1, 1, 0, 0};
+    static int dc[] = {0, 0, -1, 1};
+    static int bfs() {
+        Queue<Point> q = new LinkedList<>();
+        q.add(new Point(0, 0, 0));
+        visited[0][0] = true;
+
+        while (!q.isEmpty()) {
+            Point cur = q.poll();
+            int r = cur.r;
+            int c = cur.c;
+            for (int d = 0; d < 4; d++) {
+                int nr = r + dr[d];
+                int nc = c + dc[d];
+                if (nr < 0 || nc < 0 || nr >= N || nc >= M || visited[nr][nc]) continue;
+                if (nr == N-1 && nc == M-1) return cur.cnt+1;
+                if (mat[nr][nc]) {
+                    visited[nr][nc] = true;
+                    q.add(new Point(nr, nc, cur.cnt+1));
+                }
+            }
+        }
+
+        return -1;
+    }
+}
